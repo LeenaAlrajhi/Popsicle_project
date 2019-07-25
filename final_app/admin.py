092a6d61716caedee6d0800b9cf9a4d0fc4d7799
Leena Alrajhi <L.A.R@windowslive.com>
2019-07-25 22:09:29 +0300

    install authentication

--- final_app/admin.py
+++ final_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Popsicle, Profile
+from .models import Popsicle, Profile, Order, OrderProduct
 
 admin.site.register(Popsicle)
 admin.site.register(Profile)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
